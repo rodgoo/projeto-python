21bdb332ccbc81bb07d393c396c6c02bd7c3538b
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-07 20:48:39 -0300

    Conversor e teste lógico criados

--- conversor-de-temperatura.py
+++ conversor-de-temperatura.py
@@ -1,5 +1,5 @@
 temperaturaOriginal = float(input("A temperatura original em (ºC) será convertida para (ºF) | Qual o temperatura original: "))
 
-toF = ((9*temperaturaOriginal)/5) + 32
+F = ((9*temperaturaOriginal)/5) + 32
 
-print(f'A temperatura informada de {temperaturaOriginal}ºC é de {toF}ºF')
+print(f'A temperatura informada de {temperaturaOriginal}ºC é de {F}ºF')
