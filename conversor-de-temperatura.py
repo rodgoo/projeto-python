de317edc0499bf3ef0a0cf74a505954d9b19d788
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-07 20:46:53 -0300

    Descreva o que você mudou

---
+++ conversor-de-temperatura.py
@@ -0,0 +1,5 @@
+temperaturaOriginal = float(input("A temperatura original em (ºC) será convertida para (ºF) | Qual o temperatura original: "))
+
+toF = ((9*temperaturaOriginal)/5) + 32
+
+print(f'A temperatura informada de {temperaturaOriginal}ºC é de {toF}ºF')
---
+++ teste-logico-par-ou-impar.py
@@ -0,0 +1,8 @@
+sobrateste = int(input('Digita um número e vou te falar se é par ou ímpar'))
+
+
+if sobrateste%2 == 0:
+    print('Par')
+else:
+    print('Impar')
+
