d2caaa6cf82cd1877e12bbec1df09e398c7f9028
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-08 01:34:55 -0300

    realizado cálculo de hipotenusa

---
+++ cateto-hipotenusa-triangulo-e-codigo.py
@@ -0,0 +1,6 @@
+
+Hipotenusa = (catetoA**2 + catetoO**2)**(1/2)
+Hipotenusa1 = math.hypot(catetoO, catetoA)
+
+print(f'[Resultado sem importação(nativo)] A hipotenusa vai medir {Hipotenusa:.2f}')
+print(f'[Resultado com importação de (math)] A hipotenusa vai medir {Hipotenusa1:.2f}')
