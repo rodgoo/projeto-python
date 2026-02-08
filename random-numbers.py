acc12cd4e788e283616e057cbe48ca1e2734f35a
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-08 00:01:32 -0300

    código atualizado

---
+++ random-numbers.py
@@ -0,0 +1,8 @@
+
+# random gera um número aleatório entre 0 e 1 (float number por padrão)
+num = random.random()
+print(num)
+
+# random com um int e range definidos gera um número inteiro entre os números definidos
+numInteiro = random.randint(1,10)
+print(numInteiro)
