d30ba4544b7289fa68de699a58e33082d37a4abd
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-08 14:35:42 -0300

    Interatividade adicionada ao reprodutor MP3

--- reprodutor-mp3.py
+++ reprodutor-mp3.py
@@ -2,6 +2,8 @@ import pygame
 pygame.init()
 pygame.mixer.music.load('audio-para-py-teste.mp3')
 
+input('Pressione ENTER para iniciar a música')
 pygame.mixer.music.play()
+
 input('Press Enter to stop the music...')
 pygame.event.wait()
