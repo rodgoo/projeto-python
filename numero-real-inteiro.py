b289abb5d4c7290bcbf5748be88dd16ac7b8c506
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-08 00:01:49 -0300

    Importação de bibliotecas

---
+++ numero-real-inteiro.py
@@ -0,0 +1,6 @@
+
+numeroInteiro = int(numeroReal)
+numeroInteiroBiblioteca = math.trunc(numeroReal)
+
+print(f'Forma nativa de fazer, usando (int): {numeroInteiro}')
+print(f'Forma com biblioteca integrada, usando (math.trunc): {numeroInteiroBiblioteca}')
