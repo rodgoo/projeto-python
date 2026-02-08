950316a4e1a4cf849aaec32c51c9ce91ba4c8f20
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-08 00:02:57 -0300

    import de biblioteca && raiz quadrada

---
+++ importacao-de-bibliotecas.py
@@ -0,0 +1,11 @@
+#Importa bilioteca && Solicita um número
+import math
+numeroRaiz = int(input('Digite um número: '))
+
+# Procura a raiz do número
+raiz = math.sqrt(numeroRaiz)
+
+# Imprime a resposta da raiz, mostrando o número escolhido e sua raiz
+print(f'A raiz de {numeroRaiz} é igual a {raiz:.2f}',)
+
+
