018e778d25f4c0acb11d458977d1a27a7507c907
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-08 20:26:21 -0300

    Divisão exata corrigida

--- media-teste.py
+++ media-teste.py
@@ -1,8 +1,8 @@
-p3 = int(input('[Pergunta 3]: Me diga duas notas de um aluno, calcularei e te falo a média. Valor 01:'))
-p31 = int(input('[Pergunta 3]: Valor 02:'))
+p3 = float(input('[Pergunta 3]: Me diga duas notas de um aluno, calcularei e te falo a média. Valor 01:'))
+p31 = float(input('[Pergunta 3]: Valor 02:'))
 
-mediaCalculo = (p3+p31)//2
-mediaCalculo1 = (p3+p31)/2
+mediaCalculo = (p3+p31)/2
+mediaCalculo1 = (p3+p31)//2
 
 media = print('A média encontrada foi de:',mediaCalculo)
 media = print('Média exata encontrada:',mediaCalculo1)
