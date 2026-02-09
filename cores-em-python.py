ddea109c33aa9df8cd658a991e2eeda793959a81
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-09 20:38:09 -0300

    Cores adicionadas no projeto

---
+++ cores-em-python.py
@@ -0,0 +1,4 @@
+print('\033[31m Olá Mundo -- em vermelho')
+print('\033[30;45m Olá Mundo -- em fundo rosa')
+print('\033[30;44m Olá Mundo -- em fundo azul')
+print('\033[0;32m Olá Mundo -- em verde')
