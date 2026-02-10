7f9191489a9abac1094ae440f722fde6aa6b5900
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-10 02:38:18 -0300

    Validador de idade para alistamento

---
+++ alistamento.py
@@ -0,0 +1,13 @@
+import datetime
+
+idade = int(input('Olá, me fale seu ano de nascimento?'))
+anoNascimento = datetime.datetime.now().year - idade # Procura pela idade - o ano atual do computador
+
+if anoNascimento < 18:
+    print(f'Você ainda não completou 18 anos, ainda possui {anoNascimento} anos e ainda vai se alistar')
+
+elif anoNascimento == 18:
+    print(f'Parabéns, você completou {anoNascimento} anos recentemente, é hora de se alistar!')
+
+else:
+    print(f'Você passou do tempo de se alistar!')
