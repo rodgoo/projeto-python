3357ffdea9404de0d596b20e24d91d17277610d8
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-14 00:38:51 -0300

    Comentários adicionados

--- repeticoes-while/fatorial.py
+++ repeticoes-while/fatorial.py
@@ -6,9 +6,9 @@ fatorial = 1
 print(f'Será calculado o fatorial de {msg}!')
 
 while contador > 0:
-    print(f'{contador}', end='') # mostra o número que será contado até 1
-    print(' -> ' if contador > 1 else ' = ', end='')
-    fatorial *= contador
+    print(f'{contador}', end='') # mostra o número que será contado até 1 (cai de 1 em 1 com o contador -=1)
+    print(' -> ' if contador > 1 else ' = ', end='') # exibe (->) após cada número, caso não haja número (=)
+    fatorial *= contador # calcula o fatorial
     contador -= 1
 
 print(fatorial)
