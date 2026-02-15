4c95827ea1ab3065ec3723d9f0365b2968983538
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-14 22:12:53 -0300

    Teste lógico, para se houver 999

---
+++ repeticoes-while/numeros-com-flag.py
@@ -0,0 +1,9 @@
+soma = cont = 0
+while True:
+    numero = int(input('Digite um valor (999 para parar): '))
+    if numero == 999:
+        break
+    cont += 1
+    soma += numero
+print(f'Soma dos valores: {soma}')
+print(cont)
