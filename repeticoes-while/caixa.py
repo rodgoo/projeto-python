e12342b01c368e7ca11b27e667da0f30f4be1ca4
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-16 00:04:55 -0300

    Criado projeto que calcula valores para troco

---
+++ repeticoes-while/caixa.py
@@ -0,0 +1,27 @@
+print('{:=^30}' .format(' Caixa do Ro '))
+
+valor = int(input('Informe um valor a ser sacado: '))
+
+total = valor
+cedula = 50
+totalCedula = 0
+
+while True:
+    if total >= cedula:
+        total -= cedula
+        totalCedula += 1
+    else:
+        if totalCedula > 0:
+            print(f'Total de {totalCedula} cédulas de R$ {cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalCedula = 0
+        if total == 0:
+            break
+print('Fim do programa')
+print('Volte sempre!')
+print('=' * 30)
