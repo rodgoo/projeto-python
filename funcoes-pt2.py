147914c739fddf59d45f6008bc4545cfbb4719c8
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-28 08:58:14 -0300

    Explicação de função e escopo

--- funcoes-pt2.py
+++ funcoes-pt2.py
@@ -8,3 +8,12 @@ n = 2
 print(f'No programa principal, n vale {n}')
 print('='*40)
 teste()
+
+def funcao():
+    n1 = 4
+    print('='*20)
+    print(f'N1 EM ESCOPO vale {n1}')
+
+n1 = 2
+funcao()
+print(f'N1 FORA DO ESCOPO vale {n1}')
