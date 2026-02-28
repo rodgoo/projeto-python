57c385cd55c606e4afed2b7de69cce677f82d157
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-27 23:56:13 -0300

    Comprensão de variáveis em programas e fora de programas

---
+++ funcoes-pt2.py
@@ -0,0 +1,10 @@
+def teste():
+    x = 8
+    print(f'Na função teste, n vale: {n}')
+    print(f'Na função teste, x vale: {x}')
+
+#Programa Principal
+n = 2
+print(f'No programa principal, n vale {n}')
+print('='*40)
+teste()
