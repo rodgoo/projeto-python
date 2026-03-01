a8c5b74fc631b402d2e7c818f85f09719a08f044
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-01 13:10:59 -0300

    Aprendendo sobre modularização

---
+++ modulos/modulos-e-pacotes.py
@@ -0,0 +1,10 @@
+import funcoes
+
+num = int(input('Digite um valor: '))
+fat = funcoes.fatorial(num)
+
+
+print(f'O fatorial de {num} é {fat}!')
+print('='*20)
+print(f'O dobro de {num} é {funcoes.dobro(num)}')
+print(f'O triplo de {num} é {funcoes.triplo(num)}')
