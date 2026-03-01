f1eaaf1c1d8cc42e8ae490ca0cde25cd16a232f7
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-01 16:40:21 -0300

    Redução do programa, compactando módulos

--- formatando_modulos_moeda2/teste.py
+++ formatando_modulos_moeda2/teste.py
@@ -1,7 +1,4 @@
 import moeda
 
 preco_Input = float(input('Digite o preço: '))
-print(f'A METADE de {moeda.moeda(preco_Input)} é {moeda.metade(preco_Input,True)}')
-print(f'O DOBRO de {moeda.moeda(preco_Input)} é {moeda.dobro(preco_Input,True)}')
-print(f'O AUMENTO de 10% em {moeda.moeda(preco_Input)} é {moeda.aumentar(preco_Input,10,True)}')
-print(f'A REDUÇÃO de 13% em {moeda.moeda(preco_Input)} é {moeda.diminuir(preco_Input,13,True)}')
+moeda.resumo(preco_Input, 20, 12)
