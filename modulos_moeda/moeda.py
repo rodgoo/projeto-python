f2880eace84f92c06498743eb6e4486ba0b9de93
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-01 13:52:18 -0300

    Moedas formatadas com valores

--- modulos_moeda/moeda.py
+++ modulos_moeda/moeda.py
@@ -1,15 +1,19 @@
-def aumentar(preco, taxa):
+def aumentar(preco = 0, taxa = 0):
     resultado = preco + (preco * taxa/100)
     return resultado
 
-def diminuir(preco, taxa):
+def diminuir(preco = 0, taxa = 0):
     resultado = preco - (preco * taxa/100)
     return resultado
 
-def dobro(preco):
+def dobro(preco = 0):
     resultado = preco * 2
     return resultado
 
-def metade(preco):
+def metade(preco = 0):
     resultado = preco / 2
     return resultado
+
+# Deixei padronizado em dólar propositalmente
+def moeda (preco = 0, moeda = 'U$'):
+    return f'{moeda}{preco:>.2f}'
--- modulos_moeda/teste.py
+++ modulos_moeda/teste.py
@@ -1,7 +1,7 @@
-from moeda import aumentar, diminuir, dobro, metade
+import moeda
 
 preco_Input = float(input('Digite o preço: '))
-print(f'O AUMENTO de 10% em ${preco_Input} é ${aumentar(preco_Input, 10)}')
-print(f'A DIMINUIÇÃO de 10% em ${preco_Input} é ${diminuir(preco_Input, 10)}')
-print(f'O DOBRO de ${preco_Input} é ${dobro(preco_Input)}')
-print(f'A METADE de ${preco_Input} é ${metade(preco_Input)}')
+print(f'A METADE de {moeda.moeda(preco_Input)} é {moeda.moeda(moeda.metade(preco_Input))}')
+print(f'O DOBRO de {moeda.moeda(preco_Input)} é {moeda.moeda(moeda.dobro(preco_Input))}')
+print(f'O AUMENTO de 10% em {moeda.moeda(preco_Input)} é {moeda.moeda(moeda.aumentar(preco_Input,10))}')
+print(f'A REDUÇÃO de 10% em {moeda.moeda(preco_Input)} é {moeda.moeda(moeda.diminuir(preco_Input,10))}')
