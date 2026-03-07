a0e306c89f69edffc64d98f01d2f4090daf765f3
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-07 20:54:14 -0300

    Desafio #1 Concluído! - Funcionário - POO

---
+++ PythonPOO/desafiosClasse1/classeFuncionario.py
@@ -0,0 +1,22 @@
+from rich import print
+
+class Funcionario:
+
+    def __init__(self, nome='Desconhecido', setor='Desconhecido', cargo='Desconhecido'): #Constructor
+
+    # ÁREA PARA ATRIBUTOS DE INSTÂNCIA
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+
+    # ÁREA DE MÉTODOS DE INSTÂNCIA
+    def comando(self):
+        return(f' Olá! Sou [blue]{self.nome}[/] e sou [purple]{self.setor}[/] do setor de [green]{self.cargo}[/] da empresa CursoEmVideo!')
+
+print('-'*30)
+funcionario1 = Funcionario('Rodrigo', 'Analista', 'Sistemas de Informação')
+funcionario2 = Funcionario('Vitor', 'Funcionário', 'T.I')
+print('-'*30)
+
+print(funcionario1.comando())
+print(funcionario2.comando())
