5c40d2ed0612cd89a7a6befca488ac62c80a29b6
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-07 21:47:10 -0300

    adicionado atributo de classe

--- PythonPOO/desafiosClasse1/classeFuncionario.py
+++ PythonPOO/desafiosClasse1/classeFuncionario.py
@@ -2,8 +2,9 @@ from rich import print
 
 class Funcionario:
 
-    def __init__(self, nome='Desconhecido', setor='Desconhecido', cargo='Desconhecido'): #Constructor
+    empresa = 'Curso em Video'
 
+    def __init__(self, nome='Desconhecido', setor='Desconhecido', cargo='Desconhecido'): #Constructor
     # ÁREA PARA ATRIBUTOS DE INSTÂNCIA
         self.nome = nome
         self.setor = setor
@@ -11,7 +12,7 @@ class Funcionario:
 
     # ÁREA DE MÉTODOS DE INSTÂNCIA
     def comando(self):
-        return(f' Olá! Sou [blue]{self.nome}[/] e sou [purple]{self.setor}[/] do setor de [green]{self.cargo}[/] da empresa CursoEmVideo!')
+        return(f' Olá! Sou [blue]{self.nome}[/] e sou [purple]{self.setor}[/] do setor de [green]{self.cargo}[/] da empresa [yellow]{self.empresa}[/]!')
 
 print('-'*30)
 funcionario1 = Funcionario('Rodrigo', 'Analista', 'Sistemas de Informação')
