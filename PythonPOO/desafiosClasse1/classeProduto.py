01df237de315ac6ce7510bd4f120a2806150dbc5
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-07 22:03:06 -0300

    Usando Panel (Rich)

--- PythonPOO/desafiosClasse1/classeProduto.py
+++ PythonPOO/desafiosClasse1/classeProduto.py
@@ -1,3 +1,6 @@
+from rich import print
+from rich.panel import Panel
+
 class Produto:
 
     def __init__(self, nomeProduto='Desconhecido', preço='Desconhecido'): #Constructor
@@ -7,14 +10,17 @@ class Produto:
         self.preço = preço
 
     # ÁREA DE MÉTODOS DE INSTÂNCIA
-    def comando(self):
-        return(f'Produto: {self.nomeProduto} | Valor: R$:{self.preço:,.2f}')
+    def etiqueta(self):
+        conteudo = self.nomeProduto
+        conteudo += f'{'-'*30}'
+        preçoProduto = f'R$: {self.preço:,.2f}'
+        conteudo += f' {preçoProduto}'
+        etiqueta = Panel(conteudo, title='Produto')
+        print(etiqueta)
 
 print('-'*30)
-p1 = Produto('iPhone 17 Pro', 8000)
-p2 = Produto('Macbook Pro', 16500)
+p1 = Produto('iPhone 17 Pro ', 8000)
+p2 = Produto('Macbook Pro ', 16500)
 
-print(p1.comando())
-print('-'*30)
-print(p2.comando())
-print('-'*30)
+p1.etiqueta()
+p2.etiqueta()
