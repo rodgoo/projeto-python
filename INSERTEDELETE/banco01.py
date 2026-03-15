0a7a87e6eba615504da3f02e311a9f302da3ce9c
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-03-14 21:37:54 -0300

    Criado sistema de CREATE e DELETE de dados em banco

---
+++ INSERTEDELETE/banco01.py
@@ -0,0 +1,73 @@
+import sqlite3
+from time import sleep
+
+# Conectando banco de dados no código
+conexao_banco = sqlite3.connect('meuBanco.db')
+cursor = conexao_banco.cursor()
+
+# Criando a tabela
+cursor.execute("""CREATE TABLE IF NOT EXISTS cadastroCliente
+(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+nomeUsuario TEXT NOT NULL,               
+documento INTEGER NOT NULL,
+telefone INTEGER NOT NULL)""")
+
+while True:
+    print('='*30)
+    print('Olá! Seja bem vind@!')
+    sleep(1)
+    print('Ações possíveis com os dados: [adicionar] [excluir] [listar pessoas] [sair]')
+    sleep(1.5)
+    pergunta = input('O que deseja fazer? ')
+
+    if pergunta == 'sair':
+        break
+    
+    # INSERT - Adicionando/Injetando/Alimentando dados no banco 
+    elif pergunta == 'adicionar':
+        nomeUsuario = input('Digite o seu nome:').strip().capitalize()
+        print('='*30)
+        documento = input('Digite o número do seu documento (fictício)').strip()
+        print('='*30)
+        telefone = input('Digite o número de seu telefone (fictício):')
+        print('='*30)
+
+        try:
+            # Injetando os dados no SQL
+            dados_para_SQL = 'INSERT INTO cadastroCliente (nomeUsuario, documento, telefone) VALUES (?, ?, ?)'
+            inserindoSQL = (nomeUsuario, documento, telefone)
+
+            cursor.execute(dados_para_SQL,inserindoSQL)
+            conexao_banco.commit()  
+            print('Sucesso! Dados adicionados no banco de dados')
+        
+        except Exception as erro:
+            print('='*30)
+            print(f'Erro ao adicionar os dados no banco, dá uma olhada: {erro}')
+
+
+    # DELETE - Excluindo registros do banco de dados
+    elif pergunta == 'excluir':
+        perguntaDelete = input('Digite o nome ou ID do usuário que deseja excluir:').capitalize()
+        print('='*30)
+
+        try:
+            # Deletando dados do SQL
+            deletando_do_SQL = 'DELETE FROM cadastroCliente WHERE nomeUsuario = ? OR id = ?'
+            cursor.execute(deletando_do_SQL,(perguntaDelete, perguntaDelete))
+            conexao_banco.commit()
+
+            if cursor.rowcount > 0:
+                print('Sucesso! dado excluído do banco de dados')
+                print(f'[{nomeUsuario} saiu do chat] | não existe mais!')
+            else:
+                print(f'Opa! Esse dado: [{perguntaDelete}] não existe em sistema :/')
+        
+        except Exception as erro:
+            print('='*30)
+            print(f'Erro ao excluir dados do banco, vê aí: {erro}')
+    else:
+        print('Comando incorreto ou não reconhecido, tente novamente!')
+
+# Fecha a conexão com o banco de dados, encerrando o circuito
+conexao_banco.close()
